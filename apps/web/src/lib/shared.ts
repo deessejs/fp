3e99aadc2a9f771669f602043ee6cc4bd737652c
martyy-code <nesalia.inc@gmail.com>
2026-06-09 12:15:04 +0200

    Update

--- apps/web/src/lib/shared.ts
+++ apps/web/src/lib/shared.ts
@@ -1,4 +1,4 @@
-export const appName = "@deessejs/fp";
+export const appName = "DeesseJS FP";
 export const docsRoute = "/docs";
 export const docsImageRoute = "/og/docs";
 export const docsContentRoute = "/llms.mdx/docs";
